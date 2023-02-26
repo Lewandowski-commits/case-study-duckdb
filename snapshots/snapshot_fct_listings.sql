3df6f81ba2b702bc26950b6a9a8bddd375a4c9d0
Lewandowski-commits <michalewandowski0@gmail.com>
2023-02-26 13:48:06 +0100

    move case logic back to staging table

--- snapshots/snapshot_fct_listings.sql
+++ snapshots/snapshot_fct_listings.sql
@@ -11,19 +11,7 @@
 
 }}
 
-select listing_id,
-price,
-listing_date_key,
-platform_id,
-product_type_id,
-status_id,
-user_id,
-creation_date,
-case when
-last_update_date is null
-then creation_date
-else last_update_date
-end as last_update_date
+select *
 from {{ ref('stg_cln_listings') }}
 
 {% endsnapshot %}
