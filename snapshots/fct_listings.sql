ec9c363cd8fdf2a7b023490fb4742700f49a4aeb
Lewandowski-commits <michalewandowski0@gmail.com>
2023-02-26 13:05:01 +0100

    add key, target schema and db

--- snapshots/fct_listings.sql
+++ snapshots/fct_listings.sql
@@ -2,12 +2,15 @@
 
 {{
     config(
+        unique_key = 'listing_id',
+        target_database = 'jaffle_shop',
+        target_schema='snapshots',
         strategy = 'timestamp',
         updated_at = 'last_update_date',
     )
 
 }}
 
-select * from {{ source('jaffle_shop', 'stg_cln_listings') }}
+select * from {{ ref('stg_cln_listings') }}
 
 {% endsnapshot %}
