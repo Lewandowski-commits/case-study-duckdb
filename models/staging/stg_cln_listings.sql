26af7d59a04bc8ada8d99e82e6bf3fdca72f2782
Lewandowski-commits <michalewandowski0@gmail.com>
2023-02-26 13:17:56 +0100

    always populate last_update_date

--- models/staging/stg_cln_listings.sql
+++ models/staging/stg_cln_listings.sql
@@ -1,1 +1,15 @@
-select * from {{ ref('cln_listings') }}
+select 
+listing_id,
+price,
+listing_date_key,
+platform_id,
+product_type_id,
+status_id,
+user_id,
+creation_date,
+CASE WHEN
+last_update_date IS NULL
+THEN creation_date
+ELSE last_update_date
+END as last_update_date
+ from {{ ref('cln_listings') }}
